48e0c51d26f4c5a9d336a91d17a1e8149ae4af84
Chi-teck <Chi-teck@users.noreply.github.com>
2021-04-10 22:57:56 +0500

    Add nginx

--- xdebug.sh
+++ xdebug.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ $EUID -ne 0 ]; then
-  >&2 ansi --red --bold 'This script should be run as root.'
+if [[ $EUID != 0 ]]; then
+  >&2 echo -e '\e[91mThis command should be run as root.\e[0m'
   exit 1
 fi
 
-if [ "$1" = 'on' ]; then
-  # Apache fails with "signal Segmentation fault" when reload is used.
-  phpenmod xdebug && service apache2 restart
-elif [ "$1" = 'off' ]; then
-  phpdismod xdebug && service apache2 restart
+if [[ $1 == 'on' ]]; then
+  if [[ $SERVER == 'apache' ]]; then
+    # Apache fails with "signal Segmentation fault" when reload is used.
+    phpenmod xdebug && service apache2 restart
+  else
+    phpenmod -s fpm xdebug && phpenmod -s cli xdebug && service php%PHP_VERSION%-fpm reload
+  fi
+elif [[ $1 == 'off' ]]; then
+  if [[ $SERVER == 'apache' ]]; then
+    phpdismod xdebug && service apache2 restart
+  else
+    phpdismod -s fpm xdebug && phpdismod -s cli xdebug && service php%PHP_VERSION%-fpm reload
+  fi
 else
-  >&2 echo Usage: $(basename -- "$0") '[on|off]';
+  >&2 echo 'Usage:' $(basename -- "$0") '[on|off]';
   exit 1
 fi
