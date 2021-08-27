a5086967c885986ee633147c131dc54db292467d
Chi-teck <Chi-teck@users.noreply.github.com>
2021-08-27 19:32:37 +0500

    Update xdebug configuration

--- xdebug.sh
+++ xdebug.sh
@@ -11,15 +11,15 @@ SERVER='%SERVER%'
 if [[ $1 == 'on' ]]; then
   if [[ $SERVER == 'apache' ]]; then
     # Apache fails with "signal Segmentation fault" when reload is used.
-    phpenmod xdebug && service apache2 restart
+    phpenmod -v %PHP_VERSION% xdebug && service apache2 restart
   else
-    phpenmod -s fpm xdebug && phpenmod -s cli xdebug && service php%PHP_VERSION%-fpm reload
+    phpenmod -v %PHP_VERSION% xdebug && service php%PHP_VERSION%-fpm reload
   fi
 elif [[ $1 == 'off' ]]; then
   if [[ $SERVER == 'apache' ]]; then
-    phpdismod xdebug && service apache2 restart
+    phpdismod -v %PHP_VERSION% xdebug && service apache2 restart
   else
-    phpdismod -s fpm xdebug && phpdismod -s cli xdebug && service php%PHP_VERSION%-fpm reload
+    phpdismod -v %PHP_VERSION% && service php%PHP_VERSION%-fpm reload
   fi
 else
   >&2 echo 'Usage:' $(basename -- "$0") '[on|off]';
